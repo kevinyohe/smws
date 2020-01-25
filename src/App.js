44e89052fdbec6a6dd1ec6493a17b818003e94b4
kevinyohe <kevinyohe7@yahoo.com>
2020-01-25 00:41:40 -0500

    Add css, quotebox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>S#!T MY WIFE SAYS</h1>
+
+      <div id="ct">
+  <div class="corner "id="left_top"></div>
+  <div class="corner" id="left_bottom"></div>
+  <div class="corner" id="right_top"></div>
+  <div class="corner" id="right_bottom"></div>
+  <span>Wife</span>
+  <blockquote>
+    <p><i>&ldquo;Success consists of going from failure to failure without loss of enthusiasm.&rdquo; </i></p>
+  </blockquote>
+</div>
     </div>
   );
 }
